911cc77ac50e617f788a01dfeb7aeae63d67ce4f
ChiaraBachechi <86728801+ChiaraBachechi@users.noreply.github.com>
2022-10-24 16:31:21 +0200

    Update Footways.py
    comments

--- cicleways/Elements/Footways.py
+++ cicleways/Elements/Footways.py
@@ -5,6 +5,7 @@ import argparse
 import os
 import time
 
+#generation of footways nodes from local files obtained from OSM
 
 class App:
     def __init__(self, uri, user, password):
@@ -21,6 +22,7 @@ class App:
 
     @staticmethod
     def _import_footways(tx, file):
+        #import footways from the local file obtained from OSM and elaborated using geopandas
         result = tx.run("""
                         call apoc.load.json($file) yield value as value with value.data as data unwind data as record 
                         MERGE(n:Footway {id_num : record.id_num}) 
@@ -40,6 +42,7 @@ class App:
 
     @staticmethod
     def _import_footways_in_spatial_layer(tx):
+        #add the footways nodes in the spatial layer
         result = tx.run("""
                        match(n:Footway) with collect(n) as footway UNWIND footway AS fw 
                        CALL spatial.addNode('spatial', fw) yield node return node
@@ -73,6 +76,8 @@ class App:
     
     @staticmethod
     def _find_touched_footways(tx):
+        #exploiting the infromation obtained through geopandas regarding the footways that have a common point (only one)
+        #and connects them with the 'CONTINUE_ON_FOOTWAY' relationship
         result = tx.run("""
                 match(n:Footway) where NOT isEmpty(n.touched_footways) unwind n.touched_footways as foot 
                 match(n1:Footway) where n1.id_num=foot and NOT isEmpty(n1.touched_footways) 
@@ -97,6 +102,8 @@ class App:
     
     @staticmethod
     def _find_closest_footways(tx, file):
+        #exploiting the infromation obtained through geopandas regarding the footways that are located nearby
+        #and connects them with the 'CONTINUE_ON_FOOTWAY_BY_CROSSING_ROAD' relationship
         result = tx.run("""
                 call apoc.load.json($file) yield value as value with value.data as data 
                 UNWIND data as record match (f:Footway) where f.id_num = record.id_num and NOT isEmpty(record.closest_footways)
@@ -131,30 +138,36 @@ def add_options():
 
 
 def main(args=None):
+    #connection to the graph instance
     argParser = add_options()
     options = argParser.parse_args(args=args)
     greeter = App(options.neo4jURL, options.neo4juser, options.neo4jpwd)
-
+    
+    #generate nodes from the footways in the local file
     start_time = time.time()
     greeter.import_footways(options.file_name)
     print("import footways_total.json: done")
     print("Execution time : %s seconds" % (time.time() - start_time))
-
+    
+    #insert the footways in the spatial layer
     start_time = time.time()
     greeter.import_footways_in_spatial_layer()
     print("Import the footways in the spatial layer: done")
     print("Execution time : %s seconds" % (time.time() - start_time))
-
+    
+    #add an index on the identifier of the footways
     start_time = time.time()
     greeter.add_index()
     print("Add index on id_num : done")
     print("Execution time : %s seconds" % (time.time() - start_time))
 
+    #find the footways that have a common point (only one) and connects them with a relationship
     start_time = time.time()
     greeter.find_touched_footways()
     print("Connect the footways that touches each other: done")
     print("Execution time : %s seconds" % (time.time() - start_time))
 
+    #find the footways that are located nearby and connects them with a relationship
     start_time = time.time()
     greeter.find_closest_footways(options.file_name)
     print("Connect the footways that are close to each other: done")
