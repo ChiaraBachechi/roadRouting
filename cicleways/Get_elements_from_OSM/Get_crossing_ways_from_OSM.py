372e9a1b23342982580f61d69fa88ff59c008c27
ChiaraBachechi <86728801+ChiaraBachechi@users.noreply.github.com>
2022-10-24 14:49:06 +0200

    Update Get_crossing_ways_from_OSM.py
    comments

--- cicleways/Get_elements_from_OSM/Get_crossing_ways_from_OSM.py
+++ cicleways/Get_elements_from_OSM/Get_crossing_ways_from_OSM.py
@@ -6,6 +6,10 @@ import geopandas as gpd
 import pandas as pd
 import requests
 
+#this code import from OSM the data regarding crossings represented as way elements
+#and generates a filein the path of the graph database 
+#(but do not generates nodes in the database)
+
 class App:
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
