c4cf8e04a374aa34d36bd0e8115383c2db307c12
deggolfede <53230157+deggolfede@users.noreply.github.com>
2022-10-25 12:15:37 +0200

    Add files via upload

---
+++ Cycleways_and_Footways/Subgraphs generation and connection/Nodes generation/FootCrossCreation.py
@@ -0,0 +1,265 @@
+from ast import operator
+from neo4j import GraphDatabase
+import overpy
+import json
+import argparse
+import os
+import time
+
+
+class App:
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def connect_junctions_to_footways(self, file):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._connect_junctions_to_footways, file)
+            return result
+
+    @staticmethod
+    def _connect_junctions_to_footways(tx, file):
+        tx.run("""
+                call apoc.load.json($file) yield value as value with value.data as data 
+                unwind data as record match(f:Footway) where f.id_num = record.id_num set f.foot_crosses = record.foot_cross;
+                """, file=file)
+
+        tx.run("""
+                match(f:Footway) unwind f.foot_crosses as foot_cross with f, foot_cross match(fc:JunctionFootCross) 
+                where fc.id = apoc.convert.toString(foot_cross) with f, fc merge (f)-[:CONTAINS]->(fc);
+                """)
+        
+        tx.run("""
+                match(f:Footway)-[:CONTAINS]->(fc:JunctionFootCross) with f, fc merge (fc)-[:IS_CONTAINED]->(f);  
+                """)
+
+
+
+    def connect_junctions_to_crossings(self, file):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._connect_junctions_to_crossings, file)
+            return result
+
+
+    @staticmethod
+    def _connect_junctions_to_crossings(tx, file):
+        tx.run("""
+                call apoc.load.json("crossing_ways.json") yield value as value with value.data as data 
+                unwind data as record match(b:CrossWay) where b.id_num = record.id_num set b.junction_crosses = record.junction_cross;
+                """, file=file)
+
+        tx.run("""
+                match(cw:CrossWay) unwind cw.junction_crosses as junction_cross with cw, junction_cross match(fc:JunctionFootCross) 
+                where fc.id = apoc.convert.toString(junction_cross) with cw, fc merge(cw)-[:CONTAINS]->(fc); 
+                """)
+        
+        tx.run("""
+                match(cw:CrossWay)-[:CONTAINS]->(fc:JunctionFootCross) with cw, fc merge (fc)-[:IS_CONTAINED]->(cw); 
+                """)
+
+        tx.run("""
+                match(cn:CrossNode) with cn match(fc:JunctionFootCross) where cn.osm_id = "node/" + fc.id merge (cn)<-[:IS_MAPPED]-(fc); ; 
+                """)
+
+        result = tx.run("""
+                match(cn:CrossNode)<-[:IS_MAPPED]-(fc:JunctionFootCross) with cn, fc merge (cn)-[:IS_MAPPED]->(fc);
+                """)
+
+        return result.values()
+
+    def change_of_labels(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._change_of_labels)
+            return result
+
+    @staticmethod
+    def _change_of_labels(tx):
+        tx.run("""
+                MATCH(fc:JunctionFootCross)-[:IS_CONTAINED]->(f:Footway)
+                remove fc:JunctionFootCross
+                set fc:FootCross;
+                """)
+
+        tx.run("""
+                MATCH(fc:FootCross)-[:IS_MAPPED]->(cn:CrossNode) remove fc:FootCross set fc:JunctionFootCross;
+                """)
+
+        tx.run("""
+                MATCH(jbk:JunctionFootCross) WHERE NOT EXISTS((jbk)-[:IS_CONTAINED]-()) 
+                REMOVE jbk:JunctionFootCross set jbk:RoadFootJunction, jbk.id = "roadfoot/" + jbk.id;
+                """)
+        
+        tx.run("""
+                MATCH(fc:FootCross)-[r:FOOT_ROUTE]->(fc1:FootCross) where not exists((fc1)-->(fc)) 
+                merge (fc1)-[r1:FOOT_ROUTE]->(fc) on create set r1.distance = r.distance; 
+                """)
+
+        tx.run("""
+                match(n:JunctionFootCross) set n.id = "junctionfoot/"+n.id;
+                """)
+
+        tx.run("""
+                match(n:FootCross) set n.id = "foot/"+n.id;
+                """)
+
+
+        tx.run("""
+                drop index junction_footcross_index;
+                """)
+        
+        tx.run("""
+                create index junction_footcross_index for (jfc:JunctionFootCross) on (jfc.id);
+                """)
+
+        tx.run("""
+                create index footcross_index for (fc:FootCross) on (fc.id);
+                """)
+
+        result = tx.run("""
+                create index road_footcross_index for (rfj:RoadFootJunction) on (rfj.id);
+                """)
+
+        return result.values()
+
+
+
+    def connect_to_road_junctions(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._connect_to_road_junctions)
+            return result
+
+   
+    @staticmethod
+    def _connect_to_road_junctions(tx):
+        tx.run("""
+                MATCH(rbj:RoadFootJunction), (rj:RoadJunction) where rbj.id = rj.id merge (rbj)-[:IS_THE_SAME]-(rj);
+                """)
+
+        tx.run("""
+                MATCH(rfj:RoadFootJunction)-[r:FOOT_ROUTE]->(rfj1:RoadFootJunction)-[:IS_THE_SAME]->(rj:RoadJunction) 
+                MERGE (rfj)-[r1:ROUTE]-(rj) on create set r1.distance = r.distance;
+                """)
+
+        tx.run("""
+                MATCH(rfj:RoadFootJunction)<-[r:FOOT_ROUTE]-(rfj1:RoadFootJunction)-[:IS_THE_SAME]->(rj:RoadJunction) 
+                MERGE (rj)-[r1:ROUTE]-(rfj) on create set r1.distance = r.distance;
+                """)
+
+        tx.run("""
+                MATCH(fc:FootCross)-[r:FOOT_ROUTE]->(rfj:RoadFootJunction)-[:IS_THE_SAME]->(rj:RoadJunction) 
+                MERGE (bk)-[r1:ROUTE]-(rj) on create set r1.distance = r.distance;
+                """)
+
+        tx.run("""
+                MATCH(fc:FootCross)<-[r:FOOT_ROUTE]-(rfj:RoadFootJunction)-[:IS_THE_SAME]->(rj:RoadJunction) 
+                MERGE (rj)-[r1:ROUTE]-(fc) on create set r1.distance = r.distance;
+                """)
+
+        tx.run("""
+                MATCH(jfc:JunctionFootCross)-[r:FOOT_ROUTE]->(rfj:RoadFootJunction)-[:IS_THE_SAME]->(rj:RoadJunction) 
+                MERGE (jfc)-[r1:ROUTE]-(rj) on create set r1.distance = r.distance;
+                """)
+
+        tx.run("""
+                MATCH(jfc:JunctionFootCross)<-[r:FOOT_ROUTE]-(rfj:RoadFootJunction)-[:IS_THE_SAME]->(rj:RoadJunction) 
+                MERGE (rj)-[r1:ROUTE]-(bk) on create set r1.distance = r.distance;
+                """)
+
+        tx.run("""
+                MATCH(fc:FootCross)-[r:ROUTE]-(rj:RoadJunction) 
+                where not exists((rj)-->(fc)) merge (rj)-[r1:FOOT_ROUTE]-(fc) on create set r1.distance = r.distance;
+                """)
+
+        tx.run("""
+                MATCH(fc:JunctionFootCross)-[r:ROUTE]-(rj:RoadJunction) 
+                where not exists((rj)-->(fc)) merge (rj)-[r1:FOOT_ROUTE]->(fc) on create set r1.distance = r.distance;
+                """)               
+
+        tx.run("""
+                MATCH(rfj:RoadFootJunction)-[:IS_THE_SAME]->(rj:RoadJunction) detach delete rfj;
+                """)
+
+        result = tx.run("""
+                MATCH(f:Footway)-[:CONTAINS]-(n)-[:FOOT_ROUTE|ROUTE *1..3]-(n1)<-[:CONTAINS]-(f1:Footway) 
+                where f.id_num <> f1.id_num and not exists((f)-[*1..3]-(f1)) 
+                merge (f)-[:CONTINUE_ON_FOOTWAY_BY_CROSSING_ROAD]-(f1);
+                """)
+
+        return result
+
+
+    def import_footcrosses_into_spatial_layer(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._import_footcrosses_into_spatial_layer)
+            return result
+
+    
+    @staticmethod
+    def _import_footcrosses_into_spatial_layer(tx):
+        tx.run("""
+                match(n:FootCross) with collect(n) as crossnodes UNWIND crossnodes AS cn 
+                CALL spatial.addNode('spatial', cn) yield node return node; 
+                """)
+
+        tx.run("""
+                match(n:JunctionFootCross) with collect(n) as crossnodes UNWIND crossnodes AS cn 
+                CALL spatial.addNode('spatial', cn) yield node return node; 
+                """)
+
+        result = tx.run("""
+                match(n:RoadFootJunction) with collect(n) as crossnodes UNWIND crossnodes AS cn 
+                CALL spatial.addNode('spatial', cn) yield node return node;
+                """)
+
+        return result.values()        
+
+
+
+def add_options():
+    parser = argparse.ArgumentParser(description='Insertion of POI in the graph.')
+    parser.add_argument('--neo4jURL', '-n', dest='neo4jURL', type=str,
+                        help="""Insert the address of the local neo4j instance. For example: neo4j://localhost:7687""",
+                        required=True)
+    parser.add_argument('--neo4juser', '-u', dest='neo4juser', type=str,
+                        help="""Insert the name of the user of the local neo4j instance.""",
+                        required=True)
+    parser.add_argument('--neo4jpwd', '-p', dest='neo4jpwd', type=str,
+                        help="""Insert the password of the local neo4j instance.""",
+                        required=True)
+    parser.add_argument('--nameFileFootways', '-ff', dest='file_name_footways', type=str,
+                        help="""Insert the name of the .json file containing the footways.""",
+                        required=True)
+    parser.add_argument('--nameFileCrossingWays', '-fcw', dest='file_name_crossing_ways', type=str,
+                        help="""Insert the name of the .json file containing the crossing ways.""",
+                        required=True)
+    return parser
+
+
+def main(args=None):
+    argParser = add_options()
+    options = argParser.parse_args(args=args)
+    greeter = App(options.neo4jURL, options.neo4juser, options.neo4jpwd)
+
+    greeter.connect_junctions_to_footways(options.file_name_footways)
+    print("Connecting junction bike cross to cycleways : done")
+
+    greeter.connect_junctions_to_crossings(options.file_name_crossing_ways)
+    print("Connecting junction bike cross to crossings : done")
+
+    greeter.change_of_labels()
+    print("Change the labels of JunctionBikeCross in BikeCross : done")
+
+    greeter.connect_to_road_junctions()
+    print("Connect foot and road foot cross to road junctions : done")
+
+    greeter.import_footcrosses_into_spatial_layer()
+    print("Import the bike crosses into the spatial layer : done")
+
+
+
+    return 0
+
+
+main()
---
+++ Cycleways_and_Footways/Subgraphs generation and connection/Nodes generation/JunctionFootCrossCreation.py
@@ -0,0 +1,142 @@
+import osmnx as ox
+import argparse
+from neo4j import GraphDatabase
+import os
+
+
+class App:
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def creation_graph(self, file):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._creation_graph, file)
+            return result
+
+    @staticmethod
+    def _creation_graph(tx, file):
+        result = tx.run("""
+                        CALL apoc.import.graphml($file, {storeNodeIds: true, defaultRelationshipType: 'FOOT_ROUTE'});
+                    """, file=file)
+        return result.values()
+
+    def get_path(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._get_path)
+            return result
+
+    @staticmethod
+    def _get_path(tx):
+        result = tx.run("""
+                        Call dbms.listConfig() yield name,value where name = 'dbms.directories.neo4j_home' return value;
+                    """)
+        return result.values()
+        
+    def get_import_folder_name(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._get_import_folder_name)
+            return result
+
+    @staticmethod
+    def _get_import_folder_name(tx):
+        result = tx.run("""
+                        Call dbms.listConfig() yield name,value where name = 'dbms.directories.import' return value;
+                    """)
+        return result.values()
+
+    def set_label(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._creation_label)
+            return result
+
+    @staticmethod
+    def _creation_label(tx):
+        result = tx.run("""
+                        MATCH(n)-[:FOOT_ROUTE]->(n1) set n:JunctionFootCross, n1:JunctionFootCross;
+                    """)
+        return result.values()
+
+    def set_location(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._creation_location)
+            return result
+
+    @staticmethod
+    def _creation_location(tx):
+        result = tx.run("""
+                           match(n:JunctionFootCross) set n.geometry = "POINT(" + n.x + " " + n.y + ")", 
+                           n.location = point({latitude: tofloat(n.y), longitude: tofloat(n.x)}),n.lat = tofloat(n.y), n.lon = tofloat(n.x);
+                       """)
+        return result.values()
+
+    def set_distance(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._set_distance)
+            return result
+
+    @staticmethod
+    def _set_distance(tx):
+        result = tx.run("""
+                          MATCH (n:JunctionFootCross)-[r:FOOT_ROUTE]-(n1:JunctionFootCross) SET r.distance=tofloat(r.length), r.status='active';
+                       """)
+        return result.values()
+    
+    def set_index(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._set_index)
+            return result
+
+    @staticmethod
+    def _set_index(tx):
+        result = tx.run("""
+                           create index junction_footcross_index for (bk:JunctionBikeCross) on (bk.id);
+                       """)
+        return result.values()
+
+
+def add_options():
+    parser = argparse.ArgumentParser(description='Creation of routing graph.')
+    parser.add_argument('--latitude', '-x', dest='lat', type=float,
+                        help="""Insert latitude of city center""",
+                        required=True)
+    parser.add_argument('--longitude', '-y', dest='lon', type=float,
+                        help="""Insert longitude of city center""",
+                        required=True)
+    parser.add_argument('--distance', '-d', dest='dist', type=float,
+                        help="""Insert distance (in meters) of the area to be cover""",
+                        required=True)
+    parser.add_argument('--neo4jURL', '-n', dest='neo4jURL', type=str,
+                        help="""Insert the address of the local neo4j instance. For example: neo4j://localhost:7687""",
+                        required=True)
+    parser.add_argument('--neo4juser', '-u', dest='neo4juser', type=str,
+                        help="""Insert the name of the user of the local neo4j instance.""",
+                        required=True)
+    parser.add_argument('--neo4jpwd', '-p', dest='neo4jpwd', type=str,
+                        help="""Insert the password of the local neo4j instance.""",
+                        required=True)
+    parser.add_argument('--nameFile', '-f', dest='file_name', type=str,
+                        help="""Insert the name of the .graphml file.""",
+                        required=True)
+    return parser
+
+
+def main(args=None):
+    argParser = add_options()
+    options = argParser.parse_args(args=args)
+    greeter = App(options.neo4jURL, options.neo4juser, options.neo4jpwd)
+
+
+    greeter.creation_graph(options.file_name)
+    greeter.set_label()
+    greeter.set_location()
+    greeter.set_distance()
+    greeter.set_index()
+    greeter.close()
+
+    return 0
+
+
+main()
