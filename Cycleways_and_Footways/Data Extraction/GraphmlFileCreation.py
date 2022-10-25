fad79e7ecae87e6dc507af548a20516b91bb669c
deggolfede <53230157+deggolfede@users.noreply.github.com>
2022-10-25 12:08:03 +0200

    Add files via upload

---
+++ Cycleways_and_Footways/Data Extraction/GraphmlFileCreation.py
@@ -0,0 +1,82 @@
+import osmnx as ox
+import argparse
+from neo4j import GraphDatabase
+import os
+
+
+class App:
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def get_path(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._get_path)
+            return result
+
+    @staticmethod
+    def _get_path(tx):
+        result = tx.run("""
+                        Call dbms.listConfig() yield name,value where name = 'dbms.directories.neo4j_home' return value;
+                    """)
+        return result.values()
+        
+    def get_import_folder_name(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._get_import_folder_name)
+            return result
+
+    @staticmethod
+    def _get_import_folder_name(tx):
+        result = tx.run("""
+                        Call dbms.listConfig() yield name,value where name = 'dbms.directories.import' return value;
+                    """)
+        return result.values()
+
+ 
+
+def add_options():
+    parser = argparse.ArgumentParser(description='Creation of routing graph.')
+    parser.add_argument('--latitude', '-x', dest='lat', type=float,
+                        help="""Insert latitude of city center""",
+                        required=True)
+    parser.add_argument('--longitude', '-y', dest='lon', type=float,
+                        help="""Insert longitude of city center""",
+                        required=True)
+    parser.add_argument('--distance', '-d', dest='dist', type=float,
+                        help="""Insert distance (in meters) of the area to be cover""",
+                        required=True)
+    parser.add_argument('--neo4jURL', '-n', dest='neo4jURL', type=str,
+                        help="""Insert the address of the local neo4j instance. For example: neo4j://localhost:7687""",
+                        required=True)
+    parser.add_argument('--neo4juser', '-u', dest='neo4juser', type=str,
+                        help="""Insert the name of the user of the local neo4j instance.""",
+                        required=True)
+    parser.add_argument('--neo4jpwd', '-p', dest='neo4jpwd', type=str,
+                        help="""Insert the password of the local neo4j instance.""",
+                        required=True)
+    parser.add_argument('--nameFile', '-f', dest='file_name', type=str,
+                        help="""Insert the name of the .graphml file.""",
+                        required=True)
+    return parser
+
+
+def main(args=None):
+    argParser = add_options()
+    options = argParser.parse_args(args=args)
+    greeter = App(options.neo4jURL, options.neo4juser, options.neo4jpwd)
+    path = greeter.get_path()[0][0] + '\\' + greeter.get_import_folder_name()[0][0] + '\\' + options.file_name
+    G = ox.graph_from_point((options.lat, options.lon),
+                            dist=int(options.dist),
+                            dist_type='bbox',
+                            simplify=False,
+                            network_type='all_private'
+                            )
+    ox.save_graphml(G, path)
+
+    return 0
+
+
+main()
